3d63563f4d4ac7a54332cfbdf2e03e37a7a57894
ripozna <ov455379@gmail.com>
2021-10-06 19:49:47 +0600

    fix  models

--- images/models.py
+++ images/models.py
@@ -14,7 +14,7 @@ class Images(models.Model):
     image = models.ImageField(upload_to='images')
     status=models.CharField(max_length=10, choices=STATUS_CHOICES, default=SITE)
     is_active = models.BooleanField(default=True)
-    created = models.DateTimeField()
+    created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.image
