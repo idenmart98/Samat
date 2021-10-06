528f435f507ccda0b0b896b36cf98b87aa24d18c
ripozna <ov455379@gmail.com>
2021-10-06 19:45:14 +0600

    Add models

--- images/models.py
+++ images/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.db.models.lookups import StartsWith
 
+SITE = "SITE"
+GALLERY = "GALLERY"
+KARUSEL = "KARUSEL"
+
+STATUS_CHOICES = (
+    (SITE, "Site"),
+    (GALLERY, "GAllery"),
+    (KARUSEL, "Karusel"),
+)
+class Images(models.Model):
+    image = models.ImageField(upload_to='images')
+    status=models.CharField(max_length=10, choices=STATUS_CHOICES, default=SITE)
+    is_active = models.BooleanField(default=True)
+    created = models.DateTimeField()
+
+    def __str__(self):
+        return self.image
 # Create your models here.
