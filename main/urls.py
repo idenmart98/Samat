62521eed8e568b307b4b64e1b6058863e4b2104f
Мартин Иден <idenmart07@gmail.com>
2021-10-13 18:15:01 +0600

    rest-framework

--- main/urls.py
+++ main/urls.py
@@ -19,6 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
     path('', include("blog.urls")),
     path('',include("images.urls"))
